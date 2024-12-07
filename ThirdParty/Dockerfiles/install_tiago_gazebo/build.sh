e12a3358da2dc7f194eeddb98dc59be7a39eeb22
sang <lollolha97>
2024-12-07 12:27:51 +0900

    Modify to use tiago directly on Terminal

--- ThirdParty/Dockerfiles/install_tiago_gazebo/build.sh
+++ ThirdParty/Dockerfiles/install_tiago_gazebo/build.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-IMAGE_NAME = "tiago"
+IMAGE_NAME="tiago"
 sudo docker build -t ${IMAGE_NAME} .
+
+echo "alias tiago='sudo docker run -it \
+    -e DISPLAY=\$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    tiago'" >> ~/.bashrc
+
+source ~/.bashrc
